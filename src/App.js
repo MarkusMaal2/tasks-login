82156b16f4a9f9c2f6dfc60f97a1ad0cb4ed9798
MarkusMaal2 <markus.maal@voco.ee>
2023-02-28 09:15:54 +0200

    Implement useEffect for form validation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,12 +7,21 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+      const storedUserLoginState = localStorage.getItem("isLoggedIn")
+      if (storedUserLoginState === "1") {
+          setIsLoggedIn(true)
+      }
+  }, [])
+
   const loginHandler = (email, password) => {
-    setIsLoggedIn(true);
+      localStorage.setItem("isLoggedIn", "1")
+      setIsLoggedIn(true)
   };
 
   const logoutHandler = () => {
-    setIsLoggedIn(false);
+      localStorage.removeItem("isLoggedIn")
+      setIsLoggedIn(false)
   };
 
   return (
